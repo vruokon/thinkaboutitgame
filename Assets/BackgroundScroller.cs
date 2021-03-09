3423bd9671350eb3fa6ad090b99b0c86a4ebe2f1
Viljami <vruokonen@gmail.com>
2021-03-09 20:25:41 +0200

    first bulk commit

---
+++ Assets/BackgroundScroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public new BoxCollider2D collider;
+    public new Rigidbody2D rb;
+    
+    private float width;
+    private float scrollSpeed = -1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+     collider = GetComponent<BoxCollider2D>();
+     rb = GetComponent<Rigidbody2D>();
+     
+     width = collider.size.x;
+     collider.enabled = false;
+     
+     rb.velocity = new Vector3(scrollSpeed, 0, 11);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -width*2.1)
+        {
+            Vector3 resetPosition = new Vector3(width*2.1f * 2f, 0, 11);
+            transform.position = (Vector3)transform.position + resetPosition;
+        }
+    }
+}
---
+++ Assets/TileScrolling.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScrolling : MonoBehaviour
+{
+    public new BoxCollider2D collider;
+    public new Rigidbody2D rb;
+    
+    private float width;
+    private float scrollSpeed = -1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+     collider = GetComponent<BoxCollider2D>();
+     rb = GetComponent<Rigidbody2D>();
+     
+     width = collider.size.x*1.2f;
+     Debug.Log(width);
+     collider.enabled = false;
+     
+     rb.velocity = new Vector2(scrollSpeed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -width*17)
+        {
+            Vector2 resetPosition = new Vector2(width*18 * 2f, 0);
+            transform.position = (Vector2)transform.position + resetPosition;
+            Debug.Log(transform.position);
+        }
+    }
+}
